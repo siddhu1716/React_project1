53d1d334911519e0ac5fe1943d1ff300ef9ceb1e
shivanampalli <shiva@gmail.com>
2023-06-02 14:19:06 +0530

    initial commit

---
+++ src/components/UI/UsersList.js
@@ -0,0 +1,19 @@
+import React, { useLayoutEffect } from "react";
+import Card from "./Card";
+import classes from "./UsersList.module.css";
+
+const UsersList = (props) => {
+    return (
+        <Card className={classes.users}>
+            <ul>
+                {props.users.map((user) => (
+                    <li>
+                        {user.name} {user.age} years old
+                    </li>
+                ))}
+            </ul>
+        </Card>
+    );
+}
+
+export default UsersList;
